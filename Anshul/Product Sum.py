a00487fc1e4921e061eaa7d61608e081d6f229b2
Anshul M <am@cloudify.biz>
2020-03-21 03:37:01 +0530

    New

--- Anshul/Product Sum.py
+++ Anshul/Product Sum.py
@@ -1,13 +1,14 @@
 list = [5,2,[7,-1],3,[6,[-13,8],4]]
 
-def solve(arr):
+def solve(arr,call=1):
     n=0
     for i in arr:
         if type(i)==int:
             n+=i
         else:
-            n+=solve(i)
-    return n
-
+            call+=1
+            n+=solve(i,call)
+            call-=1
+    return n*call
 
 print(solve(list))
